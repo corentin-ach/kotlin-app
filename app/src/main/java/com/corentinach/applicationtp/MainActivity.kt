2ebaee3456c70266d5e797dbb8f30edb53707fad
Corentin <34914748+corentin-ach@users.noreply.github.com>
2021-10-28 14:57:46 +0200

    Image et Button toast

--- app/src/main/java/com/corentinach/applicationtp/MainActivity.kt
+++ app/src/main/java/com/corentinach/applicationtp/MainActivity.kt
@@ -1,25 +1,35 @@
 package com.corentinach.applicationtp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var imageView: ImageView
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         imageView = findViewById(R.id.image_view)
+        button = findViewById(R.id.my_button)
+        button.setOnClickListener(this)
         loadImage()
     }
 
     fun loadImage() {
         Picasso.get()
             .load("https://images.unsplash.com/photo-1635354942686-c862da3c30a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80")
-            .resize(50, 50)
+            .resize(500, 500)
             .centerCrop()
             .into(imageView)
+    }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(this, "You click me", Toast.LENGTH_LONG).show()
     }
 }
