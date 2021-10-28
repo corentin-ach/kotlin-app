de1242d2b43e5761ba869430c3305f5c31ca11d5
Corentin <34914748+corentin-ach@users.noreply.github.com>
2021-10-28 15:10:24 +0200

    Click change image

--- app/src/main/java/com/corentinach/applicationtp/MainActivity.kt
+++ app/src/main/java/com/corentinach/applicationtp/MainActivity.kt
@@ -18,18 +18,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         imageView = findViewById(R.id.image_view)
         button = findViewById(R.id.my_button)
         button.setOnClickListener(this)
-        loadImage()
+        loadImage("https://images.unsplash.com/photo-1635354942686-c862da3c30a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80")
     }
 
-    fun loadImage() {
+    fun loadImage(url: String) {
         Picasso.get()
-            .load("https://images.unsplash.com/photo-1635354942686-c862da3c30a6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80")
+            .load(url)
             .resize(500, 500)
             .centerCrop()
             .into(imageView)
     }
 
     override fun onClick(v: View?) {
+        val link =
+            "https://images.unsplash.com/photo-1635356178382-7f5c1de673c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=773&q=80"
+        loadImage(link)
         Toast.makeText(this, "You click me", Toast.LENGTH_LONG).show()
     }
 }
